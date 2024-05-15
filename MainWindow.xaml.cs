2b51d482eae2a123f7dddbdf3edd00f5fbd4c475
Владислав Ильин <vladi5lav.ilin@yandex.ru>
2024-05-15 16:37:05 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoboticsStudents
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Manager.MainFrame = MainFrame;
+            Manager.MainFrame.Navigate(new StudentsListPage());
+        }
+        private void BtnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.GoBack();
+        }
+        private void MainFrame_ContentRendered(object sender, EventArgs e)
+        {
+            string currentPage = MainFrame.Content.GetType().Name;
+            if (currentPage == "StudentsListPage")
+            {
+                List.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#FF6066A7"));
+                Add.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#FF40446D"));
+            }
+            else if (currentPage == "StudentsAddPage")
+            {
+                List.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#FF40446D"));
+                Add.Background = new System.Windows.Media.SolidColorBrush((System.Windows.Media.Color)System.Windows.Media.ColorConverter.ConvertFromString("#FF6066A7"));
+            }
+        }
+
+        private void List_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new StudentsListPage());
+        }
+
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new StudentsAddPage());
+        }
+    }
+}
