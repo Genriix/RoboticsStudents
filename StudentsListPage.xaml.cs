011de0bfff39ea21d6b88f060114e1ab2ae380f6
Genrix XIII <vladi5lav.ilin@yandex.ru>
2024-05-16 11:44:00 +0300

    Видосики и статьи не помогают

--- StudentsListPage.xaml.cs
+++ StudentsListPage.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
 using System.IO;
+using Microsoft.Win32;
+using OfficeOpenXml;
 
 namespace RoboticsStudents
 {
@@ -25,6 +27,61 @@ namespace RoboticsStudents
         public StudentsListPage()
         {
             InitializeComponent();
+        }
+
+        private void Import_Click(object sender, RoutedEventArgs e)
+        {
+            //OpenFileDialog op = new OpenFileDialog();
+            //op.Filter = "Документ эксель(*.xlsx) | *.xlsx | Все файлы(*.*) | *.*";
+            //if (op.ShowDialog() == true)
+            //{
+            //    using (var stream = File.Open(op.FileName, FileMode.Open, FileAccess.Read))
+            //    {
+            //        using (var reader = ExcelReaderFactory.CreateReader(stream))
+            //        {
+            //            var result = reader.AsDataSet(new ExcelDataSetConfiguration{});
+            //            var dataTable = result.Tables[0];
+            //            Students.ItemsSource = dataTable.DefaultView;
+            //        }
+            //    }
+
+            //    //foreach (DataColumn column in Students.Columns)
+            //    //{
+            //    //    var binding = new Binding(column.ColumnName);
+            //    //    var dataGridColumn = new DataGridTextColumn { Header = column.ColumnName, Binding = binding };
+            //    //    Students.Columns.Add(dataGridColumn);
+            //    //}
+            //}
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Файлы Excel (*.xls;*.xlsx)|*.xls;*.xlsx|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FileInfo fileInfo = new FileInfo(openFileDialog.FileName);
+                using (ExcelPackage excelPackage = new ExcelPackage(fileInfo))
+                {
+                    ExcelWorksheet worksheet = excelPackage.Workbook.Worksheets[1];
+                    DataTable dataTable = new DataTable();
+                    foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.Columns])
+                    {
+                        dataTable.Columns.Add(firstRowCell.Text);
+                    }
+                    for (int rowNumber = 2; rowNumber <= worksheet.Dimension.Rows; rowNumber++)
+                    {
+                        var row = worksheet.Cells[rowNumber, 1, rowNumber, worksheet.Dimension.Columns];
+                        DataRow dataRow = dataTable.Rows.Add();
+                        foreach (var cell in row)
+                        {
+                            dataRow[cell.Start.Column - 1] = cell.Text;
+                        }
+                    }
+                    Students.ItemsSource = dataTable.DefaultView;
+                }
+            }
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
